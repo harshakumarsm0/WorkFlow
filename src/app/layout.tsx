655171d8a15b9255af8d31f5b910c9497d145e66
harshakumarsm0 <harshakumar@igebra.ai>
2025-07-18 17:15:28 +0530

    dir changes

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import Header from '@/components/header';
+import Footer from '@/components/footer';
+import './globals.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+} 
